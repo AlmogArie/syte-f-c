6edffdc35adfd65aefc4c408f363db0ff8f0431c
AlmogArie <almogrie@gmail.com>
2020-09-02 13:52:13 +0300

    Updates

---
+++ precache-manifest.3dc7826faee8667b1d3bea63c6d85470.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a549c3db92a6a779e0fc23c982f8fb",
+    "url": "/syte-f-c/index.html"
+  },
+  {
+    "revision": "7a1ce3315ec9ca972f5b",
+    "url": "/syte-f-c/static/css/main.5efc53b5.chunk.css"
+  },
+  {
+    "revision": "5a1c59fb5b7622ccee86",
+    "url": "/syte-f-c/static/js/2.dc9a729a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/syte-f-c/static/js/2.dc9a729a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a1ce3315ec9ca972f5b",
+    "url": "/syte-f-c/static/js/main.583be698.chunk.js"
+  },
+  {
+    "revision": "ef885b8f8381d6f29995",
+    "url": "/syte-f-c/static/js/runtime-main.5793f4ab.js"
+  },
+  {
+    "revision": "ea05c5a08e1108c834fbb5306786c2d5",
+    "url": "/syte-f-c/static/media/Jerseyletters-lP5V.ea05c5a0.ttf"
+  },
+  {
+    "revision": "1b3e3eacb7f69c66d5685b522c3e78ea",
+    "url": "/syte-f-c/static/media/syte_club_logo.1b3e3eac.png"
+  }
+]);
